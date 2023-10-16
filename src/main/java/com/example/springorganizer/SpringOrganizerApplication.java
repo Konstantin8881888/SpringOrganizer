e72370930c7dbb8df57eec40971a75cecd541142
Konstantin8881888 <kamagoroff@gmail.com>
2023-10-16 15:38:38 +0300

    Init

---
+++ src/main/java/com/example/springorganizer/SpringOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springorganizer/SpringOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
